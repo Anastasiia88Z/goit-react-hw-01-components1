ddfae25890e9ca3f084b8b5fa5013886f5607743
Anastasiia Zhylina <anastasiiazhylina@gmail.com>
2021-08-25 19:10:38 +0200

    add propTypes for profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 const Profile = ({ name, tag, location, avatar, stats }) => {
   return (
     <div className="profile">
@@ -24,6 +26,18 @@ const Profile = ({ name, tag, location, avatar, stats }) => {
       </ul>
     </div>
   );
+};
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
 };
 
 export default Profile;
